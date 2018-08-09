c83bfba464f9e67a7af014eddd577234579c6603
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2018-08-09 17:26:31 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var request = require('request');
+var cryptoRandomString = require('crypto-random-string');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var url = req.query.url;
+  request(url, function (error, response, body) {
+    var escapedBody = body.replace(/\n/g,'\\n');
+    res.type('application/javascript'); 
+    res.render('index', { json: escapedBody, unique: cryptoRandomString(10) });
+  });
+
+});
+
+module.exports = router;
